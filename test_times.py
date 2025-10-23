f584a3291ad5fc4ac58cd07bf31e66d57e05de0f
Ella Turtle <ucaptur@ucl.ac.uk>
2025-10-23 14:54:53 +0100

    Adding the changes to make the test accurate.

--- test_times.py
+++ test_times.py
@@ -4,7 +4,7 @@ from times import time_range, compute_overlap_time
 # Move the content from the `if __name__ ...` block from `times.py` to a function called `test_given_input` into `test_times.py`
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00")
     result = compute_overlap_time(large, short)
     expected = [("2010-01-12 10:30:00", "2010-01-12 10:45:00")]
     assert result == expected
